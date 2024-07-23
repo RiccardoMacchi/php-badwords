5d03e5005d5b8baec7bd7ddc83779520d91f15e7
RiccardoMacchi <rick.mac97@outlook.com>
2024-07-23 16:00:40 +0200

    aggiunto form in html e logica php per rimozione parola da testo con stampa in pagina del nuovo testo

--- corrector.php
+++ corrector.php
@@ -1,4 +1,11 @@
 <?php
+
+$testo= $_POST['testo'];
+
+$word= $_POST['word'];
+
+$new_text= str_replace($word,'****',$testo);
+
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +16,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h3>Il tuo testo senza la parola <?php echo $word; ?></h3>
+    <p><?php echo $new_text; ?></p>    
 </body>
 </html>
